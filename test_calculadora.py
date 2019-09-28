2dc8c5ce3c2c117b41c7370d7367545d97c59f6d
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 14:12:34 -0500

    Rojo: Se agrega test para cadena con un numero

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,3 +5,7 @@ import unittest
 class TestCalculadora(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(calculadora.Calculadora.suma(''), 0, "caso vacio")
+
+    def test_suma_un_numero(self):
+        self.assertEqual(calculadora.Calculadora.suma("7"),
+                         7, "caso string con un número")
