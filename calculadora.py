b768bbceb272b7a533508e3a686abc167f15e8d8
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 12:44:39 -0500

    primer commit

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+# Ejercicio de calculadora para Kata simple
+# la calculadora procesa un string en el que
+# vienen 0, 1 o 2 números separados por coma
+# si vienen 2 números devuelve la suma de ellos
+# si viene solo un número devuelve ese número
+# si el string es vacío devuelve 0
+
+
+class Calculadora:
+    def suma(cadena):
+        pass
