04e6404ccc676d5326df045ed1e0da4f806621d5
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 14:22:08 -0500

    Rojo: Se agregan tests con diferentes numeros para strings con un solo numero

--- test_calculadora.py
+++ test_calculadora.py
@@ -9,3 +9,11 @@ class TestCalculadora(unittest.TestCase):
     def test_suma_un_numero(self):
         self.assertEqual(calculadora.Calculadora.suma("7"),
                          7, "caso string con un número")
+
+    def test_suma_un_numero_varios(self):
+        self.assertEqual(calculadora.Calculadora.suma("5"),
+                         5, "caso string con un número")
+        self.assertEqual(calculadora.Calculadora.suma("10"),
+                         10, "caso string con un número")
+        self.assertEqual(calculadora.Calculadora.suma("77"),
+                         77, "caso string con un número")
