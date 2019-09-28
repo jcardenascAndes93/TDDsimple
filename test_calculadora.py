f881d7764a290bba238a9da1a8d6967bc8cb4277
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 14:34:01 -0500

    Rojo: Agrega test para una cadena de dos numeros

--- test_calculadora.py
+++ test_calculadora.py
@@ -17,3 +17,7 @@ class TestCalculadora(unittest.TestCase):
                          10, "caso string con un número")
         self.assertEqual(calculadora.Calculadora.suma("77"),
                          77, "caso string con un número")
+
+    def test_suma_dos_numeros(self):
+        self.assertEqual(calculadora.Calculadora.suma(
+            "1, 3"), 4, "caso dos números")
